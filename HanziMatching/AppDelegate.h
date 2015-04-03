7222d9ca25d4f3e3f23694638a89776d20fa0b9e
Hao Zheng <hzheng19@illinois.edu>
2015-04-03 10:18:14 -0500

    Initial Commit

---
+++ HanziMatching/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HanziMatching
+//
+//  Created by Hao Zheng on 4/3/15.
+//  Copyright (c) 2015 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HanziMatching/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  HanziMatching
+//
+
+//  Copyright (c) 2015 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
