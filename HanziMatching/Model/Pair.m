e30e259a881edc187650de72a5bec235e76fc26a
Hao Zheng <hzheng19@illinois.edu>
2015-04-08 22:48:07 -0500

    add pair.m

---
+++ HanziMatching/Model/Pair.m
@@ -0,0 +1,32 @@
+//
+//  Pair.m
+//  HanziMatching
+//
+//  Created by Zhili Feng on 4/8/15.
+//  Copyright (c) 2015 Zhili Feng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Pair.h"
+
+@implementation Pair
+
+-(id) init
+{
+    self = [super init];
+    return self;
+}
+
+-(id) initWithFirst:(int)a Second:(int)b
+{
+    self = [super init];
+    if (self)
+    {
+        self.first = a;
+        self.second = b;
+    }
+    
+    return self;
+}
+
+@end
